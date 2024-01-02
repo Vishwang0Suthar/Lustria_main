99700d4c2531cb25e036c45d4e5d2924bf826a6e
patel parth <you@example.com>
2024-01-02 15:29:29 +0530

    blackfooter

--- src/app/componenets/common_button/common_btn.tsx
+++ src/app/componenets/common_button/common_btn.tsx
@@ -7,12 +7,12 @@ id:String}
 
 const CommonButton = ({children,id}: Props) => {
   return (
-    <div className='container self-center min-w-60 px-0 flex justify-center items-center'>
+    <div className='container self-center min-w-60 flex justify-center items-center'>
     <button className={clsx(' py-3 px-10  cursor-pointer flex gap-3 transition-all duration-300 text-center items-center text-lg  ',
     {
     'bg-white hover:bg-black border-[1px] hover:text-white border-black justify-center':id==='shopallplants',
     'bg-[#7fa15a] hover:bg-white text-white hover:text-black border-[#7fa15a] ':id==='addtocart',
-    'bg-[#7fa15a] hover:bg-slate-800  ':id==='subscribe',
+    'bg-[#7fa15a] hover:bg-slate-800 w-full justify-center ':id==='subscribe',
     'bg-[#7fa15a] w-60 justify-center hover:bg-[#fefefe] hover:text-black  hover:border-black border-[1px] border-[#7fa15a]':id==='viewcart',
     'bg-black hover:bg-white text-white w-60 justify-center hover:text-black border-black border-[1px] hover:border-[#7fa15a]':id==='checkout'
     })}>{children}</button>

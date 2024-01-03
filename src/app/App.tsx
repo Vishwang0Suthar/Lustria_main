f35723802926b9e63decd0c09f2f31ef350aaa9b
Aryanrathod21 <aryanrathod410@gmail.com>
2024-01-03 16:25:20 +0530

    component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,6 +15,9 @@ import Slide from './modules/swiper/slide'
 import Demo from './demo/page'
 import Greentext from './componenets/gtext/gtextt/gtextt'
 import Plantslide from './componenets/plants/plantslide'
+import Homeproducts from './modules/Homeproducts/Homeproducts'
+import Newarrivels from './modules/Newproducts/Newproducts'
+import Newproducts from './modules/Newproducts/Newproducts'
 
 type Props = {}
 
@@ -22,14 +25,9 @@ const App = (props: Props) => {
   return (
     <div className='flex flex-col'>
         <Banner/>
-        <section >
-        <Newarrival/>
-        {/* <Plants/>
-        <Plantslide/> */}
-        <Slide/></section>
+        <Homeproducts/> 
         <Imagecategory/>
-        <Gtext/>
-        <Newcol/>
+        <Newproducts/>
         <CommonButton id="shopallplants"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>
